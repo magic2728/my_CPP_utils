a5e2bae46163998e6e28a9aa4ddeb2d640eed1b5
tracytian(田汸泽寰) <tracytian@tencent.com>
2017-07-31 17:29:41 +0800

    keep same with tencent of my utils library

--- tracytian_utils.cc
+++ tracytian_utils.cc
@@ -1,6 +1,7 @@
-#include "tracytian_utils.h"
+#include "tracytian_utils.h"// the include and using lines before are all for the declaration reliance
+// all the variable or functions declared in namespace should also defined by giving the namespace field operator::, however, when using them, you can only say using namespace XXX to leave out the namespace field operation
 
-bool ck_open(FILE ** FH, const string &file_path, const char * mode, const char * file, const int &line, const char * func) {//assemble them together to the tracytian_util class, and the declaration and definition should be divided for avoiding multi-definition
+bool tracytian_utils::ck_open(FILE ** FH, const string &file_path, const char * mode, const char * file, const int &line, const char * func) {//assemble them together to the tracytian_util class, and the declaration and definition should be divided for avoiding multi-definition
 	* FH = fopen(file_path.c_str(), mode);
 	if (! * FH) {
 		cerr << "Cannot open " << file_path << " in " << mode << " mode!" << endl;
@@ -11,10 +12,65 @@ bool ck_open(FILE ** FH, const string &file_path, const char * mode, const char 
 	}
 }
 
-string bin_path() {
+bool tracytian_utils::ck_open(const string &file_path, fstream &FH, ios_base::openmode mode) {
+  FH.open(file_path, mode);
+  if (!FH.is_open()) {
+    cerr << "Cannot open file " << file_path << endl;
+    return false;
+  } else {
+    return true;
+  }
+}
+
+string tracytian_utils::bin_path() {
 	char bin_path[MAX_SIZE], * p;
 	readlink("/proc/self/exe", bin_path, MAX_SIZE);
 	p = strrchr(bin_path, '/');
 	* p = '\0';
 	return string(bin_path);
+}
+
+char * tracytian_utils::itoa(int num,char * str,int radix) {/*索引表*/// the default value of radix can not presented in definition if has been declared
+	char index[]="0123456789ABCDEF";
+	unsigned unum;/*中间变量*/
+	int i=0,j,k;
+	/*确定unum的值*/
+	if(radix==10&&num<0)/*十进制负数*/
+	{
+		unum=(unsigned)-num;
+		str[i++]='-';
+	}
+	else unum=(unsigned)num;/*其他情况*/
+	/*转换*/
+	do{
+		str[i++]=index[unum%(unsigned)radix];
+		unum/=radix;
+	}while(unum);
+	str[i]='\0';
+	/*逆序*/
+	if(str[0]=='-')k=1;/*十进制负数*/
+	else k=0;
+	char temp;
+	for(j=k;j<=(i-1)/2;j++)
+	{
+		temp=str[j];
+		str[j]=str[i-1+k-j];
+		str[i-1+k-j]=temp;
+	}
+	return str;
+}
+
+bool tracytian_utils::split(const string & input_str, const string& regex, vector<string> * const split_list) {
+  std::regex reg(regex);
+  sregex_token_iterator
+    first{input_str.begin(), input_str.end(), reg, - 1}, last;
+  * split_list = {first, last};
+  return true;
+}
+
+bool tracytian_utils::chomp(string const &line, char const &end, string * const res_line) {
+  uint32_t pos = line.length();
+  while (line[-- pos] == end){}
+  * res_line = line.substr(0, pos + 1);
+  return true;
 }
--- tracytian_utils.h
+++ tracytian_utils.h
@@ -1,14 +1,31 @@
 #ifndef TRACY_TIAN_UTILS_H_
 #define TRACY_TIAN_UTILS_H_
+//special add here 2 ^ 30
+#define MAX_MAX_SIZE 1073741824
 #define MAX_SIZE 65535
+#define SIZE 1024
 #define OPEN_FILE_INFO  __FILE__, __LINE__, __FUNCTION__
 #include <stdio.h>
 #include <iostream>
 #include <string>
 #include <string.h>
+#include <unistd.h>
+#include <stdlib.h>// special add here
+#include <regex>//special add here
+#include <iomanip>//special add here
+#include <math.h>//special add here
+#include <fstream>//special add here
+#include <ctime>//special add here
 using namespace std;
 
-bool ck_open(FILE ** FH, const string &file_path, const char* mode, const char * file, const int &line, const char * func);
-string bin_path();
-
+namespace tracytian_utils {
+    bool ck_open(FILE ** FH, const string &file_path, const char* mode, const char * file, const int &line, const char * func);
+    bool ck_open(const string &file_path, fstream &FH, ios_base::openmode mode);// special add here
+    string bin_path();
+    char * itoa(int num,char * str,int radix = 10);
+    // special added here string utils added
+    bool split(const string & input_str, const string& regex, vector<string> * const split_list);
+    bool chomp(string const &line, char const &end, string * const res_line);
+}
+using namespace tracytian_utils;
 #endif// TRACY_TIAN_UTILS_H_ ending tag
